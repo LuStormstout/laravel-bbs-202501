65267f76a639dd0d15882dc93fad47c3d2cb8c5f
Lu Stormstout <lustormstout@gmail.com>
2025-02-25 21:55:44 +0900

    显示个人信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,11 +12,11 @@
                      src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600"
                      alt="{{ $user->name }}">
                 <div class="card-body">
-                    <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <h5><strong>Introduction</strong></h5>
+                    <p>{{ $user->introduction }}</p>
                     <hr>
-                    <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <h5><strong>Joined on</strong></h5>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
@@ -31,7 +31,7 @@
             {{-- 用户发布的内容 --}}
             <div class="card ">
                 <div class="card-body">
-                    暂无数据 ~_~
+                    No content available.
                 </div>
             </div>
 
