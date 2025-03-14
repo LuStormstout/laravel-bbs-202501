d1fda0f964d5c4ebae6f6b857ff5a26ee275e2ba
Lu Stormstout <lustormstout@gmail.com>
2025-03-14 21:23:43 +0900

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 
 class PagesController extends Controller
@@ -17,5 +19,19 @@ class PagesController extends Controller
     public function root(): Factory|View|Application
     {
         return view('pages.root');
+    }
+
+    /**
+     * Permission denied page
+     *
+     * @return Factory|View|Redirector|RedirectResponse|Application
+     */
+    public function permissionDenied(): View|Factory|Redirector|RedirectResponse|Application
+    {
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+
+        return view('pages.permission_denied');
     }
 }
